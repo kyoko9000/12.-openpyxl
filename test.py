8e22689e736ee2403089234ca6fc64b301c3e32e
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-16 09:29:04 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy
 from openpyxl import load_workbook
 import numpy as np
 
-wb = load_workbook('database.xlsx')
+wb = load_workbook('database.xlsx', data_only=True)
 
 # in ra gia tri trong mot hang////
 ws = wb['Tom']
